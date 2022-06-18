80d908be0fc5f581fb77427dc64f0acf18f1308d
Drew Lytle <aml3ed@virginia.edu>
2022-06-18 12:18:17 -0400

    Add forwardRef to Modal and ModalForm

--- app/lib/remix-modals/Modal/Modal.tsx
+++ app/lib/remix-modals/Modal/Modal.tsx
@@ -1,25 +1,33 @@
+/* eslint-disable react/display-name */
 import type { MouseEvent } from "react";
-import { useEffect, useRef } from "react";
+import { forwardRef, useEffect } from "react";
 
-type ModalProps = React.RefAttributes<HTMLFormElement>;
-
-export const Modal: React.FC<ModalProps> = ({ children, ...props }) => {
-  const ref = useRef<HTMLDialogElement>(null);
-  useEffect(() => {
-    let current = ref.current;
-    if (current) {
-      current.showModal();
-    }
-    return () => {
-      if (current) current.close();
-    };
-  }, []);
-  return (
-    <dialog {...props} ref={ref} onClick={clickHandler}>
-      {children}
-    </dialog>
-  );
+type ModalProps = React.HTMLAttributes<HTMLDialogElement> & {
+  closeOffClick?: boolean;
 };
+
+export const Modal = forwardRef<HTMLDialogElement, ModalProps>(
+  ({ children, closeOffClick = true, ...props }, ref) => {
+    useEffect(() => {
+      let dialog = null as HTMLDialogElement | null;
+      if (document) {
+        dialog = document.querySelector("dialog");
+        if (dialog) dialog.showModal();
+      }
+      return () => {
+        if (dialog) dialog.close();
+      };
+    }, []);
+
+    const onClick = closeOffClick ? clickHandler : undefined;
+
+    return (
+      <dialog ref={ref} onClick={onClick} {...props}>
+        {children}
+      </dialog>
+    );
+  }
+);
 
 const clickHandler = (e: MouseEvent<HTMLDialogElement>) => {
   const target = e.target as HTMLDialogElement;
--- app/lib/remix-modals/ModalForm/ModalForm.tsx
+++ app/lib/remix-modals/ModalForm/ModalForm.tsx
@@ -1,21 +1,22 @@
+/* eslint-disable react/display-name */
 import type { FormProps } from "@remix-run/react";
 import { Form, useLocation } from "@remix-run/react";
+import { forwardRef } from "react";
 
 type ModalFormProps = FormProps & React.RefAttributes<HTMLFormElement>;
 
-export const ModalForm: React.FC<ModalFormProps> = ({
-  children,
-  action,
-  ...props
-}) => {
-  const location = useLocation();
+export const ModalForm = forwardRef<HTMLFormElement, ModalFormProps>(
+  ({ children, action, ...props }, ref) => {
+    const location = useLocation();
 
-  return (
-    <Form
-      action={`${action || location.pathname}${location.search}`}
-      {...props}
-    >
-      {children}
-    </Form>
-  );
-};
+    return (
+      <Form
+        ref={ref}
+        action={`${action || location.pathname}${location.search}`}
+        {...props}
+      >
+        {children}
+      </Form>
+    );
+  }
+);
